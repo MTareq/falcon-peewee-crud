9ba600ca14d2c9ed98ca9ee15fa8a6b23a1e51bd
MTareq <m.tareq89@gmail.com>
2018-01-28 17:26:12 +0200

    Add Restaurant schema for later serialization and deserialization

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,4 +1,6 @@
 from peewee import SqliteDatabase, Model, CharField, TimeField
+from marshmallow import Schema, fields
+
 db = SqliteDatabase('dh-task.db')
 
 
@@ -13,10 +15,21 @@ class Restaurant(Model):
             closes_at: datetime.time structure
                        representing when the Restaurant closes.
     """
+
     class Meta:
         "Locks this tabel to be written on the specified database"
+
         database = db
 
     name = CharField()
     opens_at = TimeField()
     closes_at = TimeField()
+
+
+class RestaurantSchema(Schema):
+    "Marshmallow schema to mirror the Restaurant model"
+
+    id = fields.int(dump_only=True)
+    name = fields.Str()
+    opens_at = fields.Time()
+    closes_at = fields.Time()
